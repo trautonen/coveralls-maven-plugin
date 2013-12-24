5a8641102437f5c72b631c62e06582ba5131b21a
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-12-24 14:39:16 +0100

    * Prepare for Saga code coverage report

---
+++ src/main/java/org/eluder/coveralls/maven/plugin/saga/SagaMojo.java
@@ -0,0 +1,30 @@
+package org.eluder.coveralls.maven.plugin.saga;
+
+import java.io.File;
+
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.eluder.coveralls.maven.plugin.AbstractCoverallsMojo;
+import org.eluder.coveralls.maven.plugin.CoverageParser;
+import org.eluder.coveralls.maven.plugin.domain.SourceLoader;
+
+/**
+ * <a href="http://timurstrekalov.github.io/saga/">Saga</a> code coverage
+ *
+ * @author Jakub Bednář (24/12/2013 14:34)
+ */
+@Mojo(name = "saga", threadSafe = false)
+public class SagaMojo extends AbstractCoverallsMojo
+{
+    /**
+     * File path to Saga coverage file.
+     */
+    @Parameter(property = "coverageFile", defaultValue = "${project.build.directory}/saga-coverage/total-coverage.dat")
+    protected File coverageFile;
+
+    @Override
+    protected CoverageParser createCoverageParser(final SourceLoader sourceLoader)
+    {
+        return null;
+    }
+}
