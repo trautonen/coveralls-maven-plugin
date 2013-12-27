453735fc68a2529c15ef4448320855324b382edc
bednar@borek-laptop <jakub.bednar@gmail.com>
2013-12-27 11:53:50 +0100

    + Chain goal

--- src/main/java/org/eluder/coveralls/maven/plugin/chain/ChainMojo.java
+++ src/main/java/org/eluder/coveralls/maven/plugin/chain/ChainMojo.java
@@ -1,7 +1,83 @@
 package org.eluder.coveralls.maven.plugin.chain;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.eluder.coveralls.maven.plugin.AbstractCoverallsMojo;
+import org.eluder.coveralls.maven.plugin.CoverageParser;
+import org.eluder.coveralls.maven.plugin.ProcessingException;
+import org.eluder.coveralls.maven.plugin.SourceCallback;
+import org.eluder.coveralls.maven.plugin.cobertura.CoberturaParser;
+import org.eluder.coveralls.maven.plugin.domain.SourceLoader;
+import org.eluder.coveralls.maven.plugin.jacoco.JaCoCoParser;
+import org.eluder.coveralls.maven.plugin.json.JsonWriter;
+import org.eluder.coveralls.maven.plugin.saga.SagaParser;
+
 /**
  * @author Jakub Bednář (27/12/2013 09:57)
  */
-public class ChainMojo {
+@Mojo(name = "chain", threadSafe = false)
+public class ChainMojo extends AbstractCoverallsMojo {
+
+    /**
+     * File path to Cobertura coverage file.
+     */
+    @Parameter(property = "coberturaFile")
+    protected File coberturaFile;
+
+    /**
+     * File path to JaCoCo coverage file.
+     */
+    @Parameter(property = "jacocoFile")
+    protected File jacocoFile;
+
+    /**
+     * File path to Saga coverage file.
+     */
+    @Parameter(property = "sagaFile")
+    protected File sagaFile;
+
+    /**
+     * @see org.eluder.coveralls.maven.plugin.saga.SagaMojo#deployedDirectoryName
+     */
+    @Parameter(property = "deployedDirectoryName", required = true, defaultValue = "src/")
+    protected String deployedDirectoryName;
+
+    @Override
+    protected CoverageParser createCoverageParser(final SourceLoader sourceLoader) {
+        return null;
+    }
+
+    @Override
+    protected void writeCoveralls(final JsonWriter writer, final SourceLoader sourceLoader, final SourceCallback sourceCallback, final CoverageParser parser) throws ProcessingException, IOException {
+
+        try {
+            getLog().info("Writing Coveralls data to " + writer.getCoverallsFile().getAbsolutePath());
+            long now = System.currentTimeMillis();
+            writer.writeStart();
+
+            if (coberturaFile != null) {
+                getLog().info("Writing from Cobertura report: " + coberturaFile.getAbsolutePath());
+                new CoberturaParser(coberturaFile, sourceLoader).parse(sourceCallback);
+            }
+
+            if (jacocoFile != null) {
+                getLog().info("Writing from JaCoCo report: " + jacocoFile.getAbsolutePath());
+                new JaCoCoParser(jacocoFile, sourceLoader).parse(sourceCallback);
+            }
+
+            if (sagaFile != null) {
+                getLog().info("Writing from Saga report: " + sagaFile.getAbsolutePath());
+                new SagaParser(sagaFile, deployedDirectoryName, sourceLoader).parse(sourceCallback);
+            }
+
+            writer.writeEnd();
+            long duration = System.currentTimeMillis() - now;
+            getLog().info("Successfully wrote Coveralls data in " + duration + "ms");
+        } finally {
+            writer.close();
+        }
+    }
 }
--- src/test/java/org/eluder/coveralls/maven/plugin/CoverageFixture.java
+++ src/test/java/org/eluder/coveralls/maven/plugin/CoverageFixture.java
@@ -38,5 +38,11 @@ public class CoverageFixture {
             // file             lines   covered lines   missed lines
             { "Components.js",  "5",    "1,2",          "" }
     };
-    
+
+    public static final String[][] COVERAGE_FILES_SAGA_COBERTURA = new String[][] {
+            // file                      lines  covered lines            missed lines
+            { "SimpleCoverage.java",     "14",  "3,6",                   "10,11" },
+            { "InnerClassCoverage.java", "30",  "3,6,9,10,12,15,18,21",  "25,26"},
+            { "Components.js",           "5",   "1,2",                   "" }
+    };
 }
