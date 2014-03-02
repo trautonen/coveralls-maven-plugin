70df3dc679a7c9ebe8ebefa2b437bc9807d825b4
trautonen <trautonen@github.com>
2014-03-02 22:41:23 +0200

    Updated license information.

--- src/main/java/org/eluder/coveralls/maven/plugin/domain/Job.java
+++ src/main/java/org/eluder/coveralls/maven/plugin/domain/Job.java
@@ -11,7 +11,7 @@ import org.eluder.coveralls.maven.plugin.validation.ValidationErrors;
  * #[license]
  * coveralls-maven-plugin
  * %%
- * Copyright (C) 2013 Tapio Rautonen
+ * Copyright (C) 2013 - 2014 Tapio Rautonen
  * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- src/main/java/org/eluder/coveralls/maven/plugin/jacoco/JaCoCoParser.java
+++ src/main/java/org/eluder/coveralls/maven/plugin/jacoco/JaCoCoParser.java
@@ -4,7 +4,7 @@ package org.eluder.coveralls.maven.plugin.jacoco;
  * #[license]
  * coveralls-maven-plugin
  * %%
- * Copyright (C) 2013 Tapio Rautonen
+ * Copyright (C) 2013 - 2014 Tapio Rautonen
  * %%
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
