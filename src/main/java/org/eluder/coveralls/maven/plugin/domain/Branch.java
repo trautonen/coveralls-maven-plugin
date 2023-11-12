3852c325fcece1ed42552ef272152a410d87a142
Jeremy Landis <jeremylandis@hotmail.com>
2023-11-12 16:35:33 -0500

    [license] Automate licensing and fix incorrect usage per java specs

--- src/main/java/org/eluder/coveralls/maven/plugin/domain/Branch.java
+++ src/main/java/org/eluder/coveralls/maven/plugin/domain/Branch.java
@@ -1,11 +1,8 @@
-package org.eluder.coveralls.maven.plugin.domain;
-
 /*
- * #[license]
- * coveralls-maven-plugin
- * %%
- * Copyright (C) 2013 - 2016 Tapio Rautonen
- * %%
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2013 - 2023, Tapio Rautonen
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -23,8 +20,8 @@ package org.eluder.coveralls.maven.plugin.domain;
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- * %[license]
  */
+package org.eluder.coveralls.maven.plugin.domain;
 
 public class Branch {
 
